fab5cc592c2434a95dc28e4b1e0ecc65f2e31b17
Itai Rozman <itairo@matrix.co.il>
2025-04-08 22:32:40 +0300

    added game file

---
+++ Models/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace ChatAppServer.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public int Cell1 { get; set; } //-3 -> +3
+        public int Cell2 { get; set; } //-3 -> +3
+        public int Cell3 { get; set; } //-3 -> +3
+        public int Cell4 { get; set; } //-3 -> +3
+        public int Cell5 { get; set; } //-3 -> +3
+        public int Cell6 { get; set; } //-3 -> +3
+        public int Cell7 { get; set; } //-3 -> +3
+        public int Cell8 { get; set; } //-3 -> +3
+        public int Cell9 { get; set; } //-3 -> +3
+
+
+    }
+}
